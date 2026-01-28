4b613522d1b80984f9d62316f03b2dd10db891ee
Anna Montoliu <amontoliusanchez@gmail.com>
2026-01-28 16:24:04 -0500

    Renombrado botón TARIFA a PRICELIST en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -68,7 +68,7 @@ export function Header({ onGoHome }) {
           onClick={handleTarifaClick}
           rel="noopener noreferrer"
         >
-          TARIFA
+          PRICELIST
         </a>
 
         {/* HOME como lo tenías */}
